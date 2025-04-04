d08f7ac8540c413074c96be732b6b26747a75049
Jakub Pekárek <78361832+KukucCZ@users.noreply.github.com>
2025-04-04 15:12:19 +0200

    feat: new version of predict and generation of validations plots for existing models

--- tracking.py
+++ tracking.py
@@ -6,10 +6,10 @@ import numpy as np
 from ultralytics import YOLO
 
 # Load the YOLO11 model
-model = YOLO("runs\\detect\\train4\\weights\\last.pt")
+model = YOLO("runs\\detect\\train19\\weights\\best.pt")
 
 # Open the video file
-video_path = "videos\\Density 1\\2024_03_01-11_00_01.mp4"
+video_path = "BuoT\\Density1\\d1_01.mp4"
 cap = cv2.VideoCapture(video_path)
 
 # Store the track history
@@ -22,7 +22,7 @@ while cap.isOpened():
 
     if success:
         # Run YOLO11 tracking on the frame, persisting tracks between frames
-        results = model.track(frame, persist=True, tracker="bytetrack.yaml")
+        results = model.track(frame, persist=True, tracker="botsort_beetle.yaml", max_det=1000, conf=0.4, iou=0.5, imgsz=1920, augment=False)
 
         # Get the boxes and track IDs
         boxes = results[0].boxes.xywh.cpu()
@@ -41,7 +41,7 @@ while cap.isOpened():
 
             # Draw the tracking lines
             points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-            cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
+            cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=2)
 
         # Display the annotated frame
         cv2.imshow("YOLO11 Tracking", annotated_frame)
